2df18048bcd5cec4c22691b44dafabc630262d3f
Mo <116412610+GreenMachineStudios@users.noreply.github.com>
2026-09-21 20:41:23 -0700

    redid project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -1,14 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.stealthrobotics.library.opmodes.StealthOpMode;
 
-@SuppressWarnings("unused")
-@TeleOp(name = "Teleop")
 public class Teleop extends StealthOpMode {
-    private final GamepadEx gamepad = new GamepadEx(gamepad1);
+    private final GamepadEx driverController = new GamepadEx(gamepad1);
 
     @Override
     public void initialize() {
